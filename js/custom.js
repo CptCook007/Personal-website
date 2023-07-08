d8dc22d4a8892a3c0db730c8dc8c6dbba8926bf4
Shamal Muneer <shamal20017@gmail.com>
2023-07-09 02:05:07 +0530

    First commit

---
+++ js/custom.js
@@ -0,0 +1,108 @@
+
+$(window).load(function () {
+    jQuery('#all').click();
+    return false;
+});
+
+$(document).ready(function () {
+    $('#header_wrapper').scrollToFixed();
+    $('.res-nav_click').click(function () {
+        $('.main-nav').slideToggle();
+        return false
+
+    });
+    $('#mainNav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 950,
+        scrollThreshold: 0.2,
+        filter: '',
+        easing: 'swing',
+        begin: function () {
+        },
+        end: function () {
+            if (!$('#main-nav ul li:first-child').hasClass('active')) {
+                $('.header').addClass('addBg');
+            } else {
+                $('.header').removeClass('addBg');
+            }
+
+        },
+        scrollChange: function ($currentListItem) {
+            if (!$('#main-nav ul li:first-child').hasClass('active')) {
+                $('.header').addClass('addBg');
+            } else {
+                $('.header').removeClass('addBg');
+            }
+        }
+    });
+
+    var container = $('#portfolio_wrapper');
+
+
+    container.isotope({
+        animationEngine: 'best-available',
+        animationOptions: {
+            duration: 100,
+            queue: false
+        },
+        layoutMode: 'fitRows'
+    });
+
+    $('#filters a').click(function () {
+        $('#filters a').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        container.isotope({
+            filter: selector
+        });
+        setProjects();
+        return false;
+    });
+
+    function splitColumns() {
+        var winWidth = $(window).width(),
+            columnNumb = 1;
+
+
+        if (winWidth > 1024) {
+            columnNumb = 4;
+        } else if (winWidth > 900) {
+            columnNumb = 2;
+        } else if (winWidth > 479) {
+            columnNumb = 2;
+        } else if (winWidth < 479) {
+            columnNumb = 1;
+        }
+
+        return columnNumb;
+    }
+
+    function setColumns() {
+        var winWidth = $(window).width(),
+            columnNumb = splitColumns(),
+            postWidth = Math.floor(winWidth / columnNumb);
+
+        container.find('.portfolio-item').each(function () {
+            $(this).css({
+                width: postWidth + 'px'
+            });
+        });
+    }
+
+    function setProjects() {
+        setColumns();
+        container.isotope('reLayout');
+    }
+
+    container.imagesLoaded(function () {
+        setColumns();
+    });
+
+
+    $(window).bind('resize', function () {
+        setProjects();
+    });
+
+    $(".fancybox").fancybox();
+});
